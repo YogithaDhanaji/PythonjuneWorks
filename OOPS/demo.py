19cb14a964ba05b6c57d747e169b72b160a39607
yogitha <yogithadhanaji26@gmail.com>
2024-09-26 06:46:58 +0530

    add

--- OOPS/demo.py
+++ OOPS/demo.py
@@ -1,1 +1,42 @@
-print("object oriented programing")
+class employee:
+
+    eid:int
+
+    name:str
+
+    age:int
+
+    gender:str
+
+    department:str
+
+
+    def set_employee(self,id,name,age,gender,dept):
+
+        self.eid=id
+
+        self.name=name
+
+        self.age=age
+
+        self.gender=gender
+
+        self.department=dept
+
+    def disply_employee(self):
+
+        print(self.age)
+
+# create onjects      
+
+
+
+employee_instanse=employee()
+
+employee_instanse.set_employee(123,"yogi","female",23,"python")
+
+employee_instanse.disply_employee()
+
+
+
+
